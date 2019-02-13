da22b37f9b47926b95c4872baa0c7af14bae3c42
LEE-HOME\lee <toxin0201@naver.com>
2019-02-13 23:15:04 +0900

    static file 처리 라우터 최상단으로 이동 npm i express-session session 미들웨어 추가

--- app.js
+++ app.js
@@ -13,16 +13,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(function(req, res, next) {
-	console.log(req.url, '저도 미들웨어 입니다');
-	next();
-});
-
 app.use(logger('dev'));
+
+// static file 라우터 - 최상단 위치, 다른 미들웨어 작업 안하도록 함
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('sbk6648'));
+app.use(session() {
+	resave : false,
+	saveUninitialized : false,
+	secret : 'sbk6648',
+	cooke: {
+		httpOnly : true,
+		secure : false,
+	}
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,7 +42,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+	console.log(err);
 	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
