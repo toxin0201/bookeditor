21948c95a16dfc59df2c62b83f24174ddd664f60
LEE-HOME\lee <toxin0201@naver.com>
2019-02-13 22:23:52 +0900

    node_module 폴더 삭제 - 버전관리 안하고 서버와 로컬 각각 npm install해서 사용 npm i pug app.js에 404, 500 미들웨어 추가

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(function(req, res, next) {
+	console.log(req.url, '저도 미들웨어 입니다');
+	next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +26,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
 
 module.exports = app;
