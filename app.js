04447ebbf8d5cbaa1964751d531863f7e3e2bb6e
LEE-HOME\lee <toxin0201@naver.com>
2019-02-17 21:24:11 +0900

    미들웨어 express 세션 수정

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('sbk6648'));
-app.use(session() {
+app.use(session({
 	resave : false,
 	saveUninitialized : false,
 	secret : 'sbk6648',
@@ -28,7 +29,7 @@ app.use(session() {
 		httpOnly : true,
 		secure : false,
 	}
-});
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
