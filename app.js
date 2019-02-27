298f4ba99150b13d8bc85698fb5cb72fddaceac0
LEE-HOME\lee <toxin0201@naver.com>
2019-02-28 00:27:33 +0900

    app.js에 sequelize 추가

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sequelize = require('./models').sequelize;
 
 var app = express();
+sequelize.sync();
 
 // view engine
 app.set('views', path.join(__dirname, 'views'));
